a6c7fcd01cab53613e33c9c132e6f6a328c3c36d
Brspontes <brian.robert16@hotmail.com>
2021-07-27 21:43:37 -0300

    finalizando pagina

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -3,11 +3,12 @@ import Header from '../../components/Header';
 import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../../services/prismic';
 import { FiUser, FiClock, FiCalendar } from "react-icons/fi";
-
 import styles from './post.module.scss';
 import format from 'date-fns/format';
 import { ptBR } from 'date-fns/locale';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { RichText } from 'prismic-dom';
+import Head from 'next/head';
 
 interface Post {
   first_publication_date: string | null;
@@ -31,54 +32,81 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
-  return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.containerHeader}>
-          <Header />
-        </div>
-      </div>
-      <div className={styles.banner}>
-        <img src={post?.data?.banner.url} alt="banner" />
-      </div>
-      <div className={styles.content}>
-        <div className={styles.contentHeader}>
-          <h1>{post?.data?.title}</h1>
-          <div className={styles.infos}>
-            <div className={styles.calendar}>
-              <FiCalendar />
-              <span>
-                {
-                  format(
-                    new Date(post?.first_publication_date),
-                    "d 'de' MMM yyyy",
-                    {
-                      locale: ptBR,
-                    }
-                  )
-                }
-              </span>
-            </div>
-            <div className={styles.user}>
-              <FiUser />
-              <span>{post?.data?.author}</span>
-            </div>
-            <div className={styles.time}>
-              <FiClock />
-              <span>4 min</span>
-            </div>
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
+  } else {
+    return (
+      <>
+        <Head>
+          <title>POST | {post.data.title}</title>
+        </Head>
+        <div className={styles.container}>
+          <div className={styles.containerHeader}>
+            <Header />
           </div>
         </div>
-      </div>
-    </>
-  )
+        <div className={styles.banner}>
+          <img src={post?.data?.banner.url} alt="banner" />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contentHeader}>
+            <h1>{post?.data?.title}</h1>
+            <div className={styles.infos}>
+              <div className={styles.calendar}>
+                <FiCalendar />
+                <span>
+                  {
+                    format(
+                      new Date(post?.first_publication_date),
+                      "d 'de' MMM yyyy",
+                      {
+                        locale: ptBR,
+                      }
+                    )
+                  }
+                </span>
+              </div>
+              <div className={styles.user}>
+                <FiUser />
+                <span>{post?.data?.author}</span>
+              </div>
+              <div className={styles.time}>
+                <FiClock />
+                <span>4 min</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.contentText}>
+          {post.data.content.map(content => (
+              <div key={content.heading}>
+                <h2>{content.heading}</h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(content.body),
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
 
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
+
   const posts = await prismic.query(
-    [Prismic.predicates.at('document.type', 'post')],
+    [Prismic.predicates.at('document.type', 'posts')],
     { pageSize: 3 }
   );
 
@@ -109,7 +137,10 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       title: response.data.title,
       banner: response.data.banner,
       author: response.data.author,
-      content: response.data.content,
+      content: response.data.content.map(item => ({
+        heading: item.heading,
+        body: [...item.body],
+      })),
     },
   };
 
